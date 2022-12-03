46a8830de8fd508f04a4c184d7afee1e8e7601a4
tangly1024 <mail@tangly1024.com>
2022-12-03 14:12:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,7 @@ const BLOG = {
   BIO: '一个追求奇怪的高中生', // 作者简介
   LINK: 'https://blog.chwin.asia', // 网站地址
   KEYWORDS: 'Notion, 博客, blog, IoriMaboroshi, 生活, 伊緻幻', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID: 'a77c044aa3114463bb97180d2eaebb4a'
-  process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  NOTION_PAGE_ID: 'a77c044aa3114463bb97180d2eaebb4a',
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
